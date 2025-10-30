10a4797eeab7636d9aadbd61b1fdd3fc7badf6eb
Ankeet1729 <ankeet1728@gmail.com>
2025-10-30 18:30:25 +0100

    Fixed the concept name change bug

--- ui/ET617_backend/db.js
+++ ui/ET617_backend/db.js
@@ -1,30 +1,30 @@
-// // db.js (ESM version)
-// import pg from 'pg';
-// const { Pool } = pg;
-
-// const pool = new Pool({
-//   user: 'kali',       // PostgreSQL username
-//   host: 'localhost',
-//   database: 'kali',
-//   password: 'qwerty',
-//   port: 5432
-// });
-
-// export default pool;
-
-// db.js (ESM) - env-driven
+// db.js (ESM version)
 import pg from 'pg';
 const { Pool } = pg;
 
 const pool = new Pool({
-  user: 'postgres',
+  user: 'kali',       // PostgreSQL username
   host: 'localhost',
-  database: 'postgres',
-  password: 'password123',
+  database: 'kali',
+  password: 'qwerty',
   port: 5432
 });
 
-
-
 export default pool;
+
+// db.js (ESM) - env-driven
+// import pg from 'pg';
+// const { Pool } = pg;
+
+// const pool = new Pool({
+//   user: 'postgres',
+//   host: 'localhost',
+//   database: 'postgres',
+//   password: 'password123',
+//   port: 5432
+// });
+
+
+
+// export default pool;
 
